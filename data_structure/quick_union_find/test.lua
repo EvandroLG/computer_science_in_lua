7b2266292ed3f0dbe538856a3bb4b4799128fd39
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-10-18 19:49:47 -0200

    simple implementation of union find

---
+++ data_structure/quick_union_find/test.lua
@@ -0,0 +1,13 @@
+package.path = './data_structure/?/?.lua;' .. package.path
+
+local UnionFind = require 'quick_union_find'
+local unittest = require 'unittest'
+local test = unittest.test
+local assert_equal = unittest.assert_equal
+
+test('create connection between 1 and 2', function()
+  local union_find = UnionFind:new(10)
+  union_find:union(1, 2)
+
+  assert_equal(union_find:is_connected(1, 2), true)
+end)
