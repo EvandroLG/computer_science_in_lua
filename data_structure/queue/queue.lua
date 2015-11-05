4f322aa03926739af1370c64b6ae3fbcd7efcc63
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-11-05 16:56:05 -0200

    implemented queue

---
+++ data_structure/queue/queue.lua
@@ -0,0 +1,53 @@
+local Queue = {}
+
+function Queue:new()
+  local obj = {}
+  self.__index = self
+  self._head = nil
+  self._tail = nil
+  self._size = 0
+
+  return setmetatable(obj, self)
+end
+
+function Queue:_create_node(item)
+  return {
+    item = item,
+    node_next = nil
+  }
+end
+
+function Queue:enqueue(item)
+  local node = self:_create_node(item)
+
+  if self._head == nil then
+    self._head = node
+  else
+    self._tail.node_next = node
+  end
+
+  self._tail = node
+  self._size = self._size + 1
+end
+
+function Queue:dequeue()
+  self._size = self._size - 1
+  local item = self._head.item
+  self._head = self._head.node_next 
+
+  if self:is_empty() then
+    self._tail = nil
+  end
+
+  return item
+end
+
+function Queue:is_empty()
+  return self._size == 0
+end
+
+function Queue:size()
+  return self._size
+end
+
+return Queue
---
+++ data_structure/queue/test.lua
@@ -0,0 +1,36 @@
+package.path = './data_structure/?/?.lua;' .. package.path
+
+local Queue = require 'queue'
+local unittest = require 'unittest'
+local test = unittest.test
+local assert_equal = unittest.assert_equal
+
+test('is_empty should return true', function()
+  local queue = Queue:new()
+  assert_equal(queue:is_empty(), true)
+end)
+
+test('is_empty should return false when it has at least one item', function()
+  local queue = Queue:new()
+  queue:enqueue('Lua')
+  assert_equal(queue:is_empty(), false)
+end)
+
+test('should return correct size', function()
+  local queue = Queue:new()
+  queue:enqueue('JavaScript')
+  queue:enqueue('Python')
+  queue:enqueue('Lua')
+
+  assert_equal(queue:size(), 3)
+end)
+
+test('should return and remove the first item', function()
+  local queue = Queue:new()
+  queue:enqueue('JavaScript')
+  queue:enqueue('Python')
+  queue:enqueue('Lua')
+
+  assert_equal(queue:dequeue(), 'JavaScript')
+  assert_equal(queue:size(), 2)
+end)
