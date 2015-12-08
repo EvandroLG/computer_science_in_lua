546c7994fe8f89a6d67e14260c28db7ab8080b9a
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-12-07 22:47:41 -0200

    added remove method on binary search class

--- data_structure/binary_search_tree/binary_search_tree.lua
+++ data_structure/binary_search_tree/binary_search_tree.lua
@@ -1,3 +1,11 @@
+local ternary = function(condition, positive, negative)
+  if condition then
+    return positive
+  end
+
+  return negative
+end
+
 local BinarySearchTree = {}
 
 function BinarySearchTree:new()
@@ -64,6 +72,107 @@ function BinarySearchTree:contains(value)
   end
 
   return found
+end
+
+function BinarySearchTree:remove(value)
+  local found = false
+  local current = self._root
+  local parent = nil
+
+  while not found and current do
+    if value < current.value then
+      parent = current
+      current = current.left
+    elseif value > current.value then
+      parent = current
+      current = current.right
+    else
+      found = true
+    end
+  end
+
+  if not found then
+    return false
+  end
+
+  local child_count = ternary(current.left ~= nil, 1, 0) + ternary(current.right ~= nil, 1, 0)
+
+  if current == self._root then
+    if child_count == 0 then
+      self._root = nil
+      return true
+    end
+
+    if child_count == 1 then
+      self._root = ternary(current.right == nil, current.left, current.right)
+      return true
+    end
+
+    if child_count == 2 then
+      local replacement = self._root.left
+      local replacement_parent = nil
+
+      while replacement.right ~= nil do
+        replacement_parent = replacement 
+        replacement = replacement.right
+      end
+
+      if replacement_parent ~= nil then
+        replacement_parent.right = replacement.left
+        replacement.right = self._root.right
+        replacement.left = self._root.left
+      else
+        replacement.right = self._root.right
+      end
+
+      self._root = replacement
+      return true
+    end
+  else
+    if child_count == 0 then
+      if current.value < parent.value then
+        parent.left = nil
+      else
+        parent.right = nil
+      end
+
+      return true
+    end
+
+    if child_count == 1 then
+      if current.value < parent.value then
+        parent.left = ternary(current.left == nil, current.right, current.left)
+      else
+        parent.right = ternary(current.left == nil, current.right, current.left)
+      end
+
+      return true
+    end
+
+    if child_count == 2 then
+      replacement = current.left
+      replacement_parent = current
+
+      while replacement.right ~= nil do
+        replacement_parent = replacement
+        replacement = replacement.right
+      end
+
+      replacement_parent.right = replacement.left
+      replacement.right = current.right
+      replacement.left = current.left
+
+      if current.value < parent.value then
+        parent.left = replacement
+      else
+        parent.right = replacement
+      end
+    end
+
+    return true
+  end
+
+
 end
 
 return BinarySearchTree
