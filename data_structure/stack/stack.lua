aca1433668ca3d2e08d846f42ebd6db4f84ddcea
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-12-17 09:20:20 -0200

    re-implemented stack and add peek method

--- data_structure/stack/stack.lua
+++ data_structure/stack/stack.lua
@@ -4,24 +4,39 @@ function Stack:new()
   local obj = {}
   self.__index = self
   self._head = nil
+  self._tail = nil
   self._size = 0
 
   return setmetatable(obj, self)
 end
 
+function Stack:peek()
+  return self._head.item
+end
+
 function Stack:_create_node(item)
   return {
     item = item,
-    node_next = nil
+    node_next = nil,
+    node_prev = nil
   }
 end
 
 function Stack:push(item)
-  local old_head = self._head
-  self._head = self:_create_node(item)
-  self._head.item = item
-  self._head.node_next = old_head
+  local node = self:_create_node(item)
+
+  if self:is_empty() then
+    self._head = node
+  else
+    local old_tail = self._tail
+    old_tail.node_next = node
+    node.node_prev = old_tail
+  end
+
+  self._tail = node
   self._size = self._size + 1
+
+  return self._size
 end
 
 function Stack:pop()
@@ -29,8 +44,10 @@ function Stack:pop()
     return nil
   end
 
-  local item = self._head.item
-  self._head = self._head.node_next
+  local item = self._tail.item
+  local new_tail = self._tail.node_prev
+  new_tail.node_next = nil
+  self._tail = new_tail
   self._size = self._size - 1
 
   return item
