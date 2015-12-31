61a606ec85cbeb424894f49e341a90d0278c3b93
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-12-31 19:28:24 -0200

    improved merge sort

--- algorithms/sorting/merge_sort/merge_sort.lua
+++ algorithms/sorting/merge_sort/merge_sort.lua
@@ -1,10 +1,8 @@
 function _slice(array, first, last)
   local output = {}
-  local i = first
 
-  while i <= last do
+  for i=first, last do
     table.insert(output, array[i])
-    i = i + 1
   end
 
   return output
@@ -20,14 +18,16 @@ end
 
 function _merge(left, right)
   local output = {}
-  local index_left = 0
-  local index_right = 0
+  local index_left = 1
+  local index_right = 1
 
   while index_left < #left and index_right < #right do
     if left[index_left] < right[index_right] then
       table.insert(output, left[index_left])
+      index_left = index_left + 1
     else
       table.insert(output, right[index_right])
+      index_right = index_right + 1
     end
   end
 
@@ -40,7 +40,7 @@ function merge_sort(items)
 
   local middle = math.floor(#items / 2)
   local left = _slice(items, 1, middle)
-  local right = _slice(items, middle, #items)
+  local right = _slice(items, middle+1, #items)
 
   return _merge(merge_sort(left), merge_sort(right))
 end
